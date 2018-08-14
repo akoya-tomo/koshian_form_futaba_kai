49bd12a2653de246d0534123f9588bcf6d9679e5
akoya-tomo <akoya_tomo-00755@yahoo.co.jp>
2018-08-14 13:47:21 +0900

    res_header.js削除

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -Ceu
 cd `dirname $0`
 
 TARGET_DIR="./koshian_form_futaba"
-OUTPUT_DIR="./"
+OUTPUT_DIR=".."
 
 script_dir=$(cd $(dirname $0); pwd)
 addon_name=${script_dir##*/}
@@ -11,7 +11,6 @@ addon_ver=$(jq ".version" ${TARGET_DIR}/manifest.json)
 filename="${OUTPUT_DIR}/${addon_name}-${addon_ver:1:-1}.zip"
 
 cd ${TARGET_DIR}
-grep -v -e '^\s*//.\+' -e '^\s*/\*' -e '^\s*\*' res_body.js | sed -e 's/\s*\/\/.\+//g' >| res_body.min.js
-cat res_header.js <(echo) encoding.min.js <(echo) res_body.min.js >| res.js
+cat res_body.js >| res.js
 
-zip -q -r -9 ${filename} * -x "*.bak" "res_*.js" "encoding*.js" ".eslint*"
+zip -q -r -9 ${filename} * -x "*.bak" "res_*.js" ".eslint*"
